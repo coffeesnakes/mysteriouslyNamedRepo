3f051b808de52b13d43e3ec2d26ed53c0e1bec02
Jacob Berg <coffeesnakes@gmail.com>
2020-11-18 20:39:07 -0800

    forEach into es6 arrow, great success!

--- thinkingEmoji.js
+++ thinkingEmoji.js
@@ -1,1 +1,5 @@
 let arrayOfNames = ["Adam", "Alex", "Aaron", "Ben", "Carl", "Dan", "David", "Edward", "Fred", "Frank", "George", "Hal", "Hank", "Ike", "John", "Jack", "Joe", "Larry", "Monte", "Matthew", "Mark", "Nathan", "Otto", "Paul", "Peter", "Roger", "Roger", "Steve", "Thomas", "Tim"]
+
+arrayOfNames.forEach((name) => {
+  console.log("name: " + name)
+}); // sick, it worked.
