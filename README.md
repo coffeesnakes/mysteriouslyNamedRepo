89e0bbe34986dd2f9f1503a5f889d5585e4b2e0c
Jacob Berg <coffeesnakes@gmail.com>
2020-11-18 20:35:58 -0800

    hardcoded dataset of names (array)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # mysteriouslyNamedRepo
+
+oh, there's nothing mysterious here...
+
+it's just practice for misc. stuff
---
+++ thinkingEmoji.js
@@ -0,0 +1,1 @@
+let arrayOfNames = ["Adam", "Alex", "Aaron", "Ben", "Carl", "Dan", "David", "Edward", "Fred", "Frank", "George", "Hal", "Hank", "Ike", "John", "Jack", "Joe", "Larry", "Monte", "Matthew", "Mark", "Nathan", "Otto", "Paul", "Peter", "Roger", "Roger", "Steve", "Thomas", "Tim"]
