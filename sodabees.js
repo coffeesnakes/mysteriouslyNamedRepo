3f8357118c10da6af452155b5e3040ee261e08af
Jacob Berg <coffeesnakes@gmail.com>
2020-11-30 23:49:53 -0800

    deep dive into ternary operator 1337Haxx0rz

--- sodabees.js
+++ sodabees.js
@@ -30,3 +30,14 @@ const ternaryFizzBuzzer = function () {
 for (let i = 1; i < 101;)
 console.log(( ++i % 3 ? '' : 'Fizz') + (i % 5 ? '': 'Buzz') || i)
 }
+
+/*
+the ternary looks weird but it's really simple.
+ternary syntax = condition ? if true : if false;
+
+ if the current *i* is divisible by 3 it will evaluate to 0 because in this instance the % is going to show the remainder of the number after being divided by whatever number is to the right of the % sign.
+ If that number evaluates to 0 it's a falsey value and will output Fizz, or Buzz respectively, if both return falsey it will concatenate them into Fizz for the first evaluate and Buzz for the second, outputing FizzBuzz.
+ If both of those check return a number that isn't 0 for the remainder that's a falsey value and in each check respectively returns an empty string which for this case is nothing, and then goes to the or ( || ) statement and will output the number that
+  *i* currently represents.
+
+*/
